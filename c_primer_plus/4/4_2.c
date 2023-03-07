3c3f426606aae87776e7841f9e1bc0c67f09e5b0
ytkz11 <ytkz11@163.com>
2023-03-07 22:31:52 +0800

    配置环境

--- c_primer_plus/4/4_2.c
+++ c_primer_plus/4/4_2.c
@@ -3,3 +3,14 @@
  * @date  2023/3/7 22:19
  * @version 1.0
 */
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char name;
+    printf("ÄãµÄÃû×Ö");
+    scanf("%s",&name);
+    printf("hello,%s",name);
+    return 0;
+}
